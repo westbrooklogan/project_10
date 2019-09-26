90d74b98a7aa024b1b84640657b3c03a47cb0af1
Logan Westbrook <westbrooklogan@gmail.com>
2019-09-26 15:13:09 -0500

    Added code to create masters, pages, master xml sheets

--- Test_App/Program.cs
+++ Test_App/Program.cs
@@ -14,13 +14,18 @@ namespace visio_test_app
 
         static void Main(string[] args)
         {
-            /* XmlDocument doc = create_Document();
-            Queue<List<Info>> cells = create_Doc_Queue();
-            XmlWriter docWriter = create_Document_Part(cells);
-            cells = create_Pages_Queue();
-            XmlWriter pagesWriter = create_Pages_Part(cells);
-            doc.Save(docWriter);
-            doc.Save(pagesWriter);*/
+            XmlDocument doc = create_Document();
+            XmlWriter writer = create_Document_Part(create_Doc_Queue());
+            doc.Save(writer);
+            writer = create_Pages_Part(create_Pages_Queue());
+            doc.Save(writer);
+            writer = create_Masters_Part(create_Masters_Queue());
+            doc.Save(writer);
+            writer = create_Master_Part(create_Master_Queue());
+            doc.Save(writer);
+            writer = create_Page_Part(create_Page_Queue());
+            doc.Save(writer);
+
             fileHandler = new FileHandler();
             dataMapHandler = new DataMapHandler();
             shapeHandler = new ShapeHandler();
@@ -105,6 +110,100 @@ namespace visio_test_app
 
             Queue<List<Info>> cells = new Queue<List<Info>>();
             cells.Enqueue(pageCells);
+
+            return cells;
+        }
+
+        private static Queue<List<Info>> create_Page_Queue()
+        {
+            List<Info> pageCells = new List<Info>()
+            {
+                new Info("PinX", "4"),
+                new Info("PinY", "5.5")
+            };
+
+            Queue<List<Info>> cells = new Queue<List<Info>>();
+            cells.Enqueue(pageCells);
+
+            return cells;
+        }
+
+        private static Queue<List<Info>> create_Masters_Queue()
+        {
+            List<Info> mastersCells = new List<Info>()
+            {
+                new Info("PageWidth", "2"),
+                new Info("PageHeight", "1"),
+                new Info("PageScale", "1"),
+                new Info("DrawingScale", "1")
+            };
+
+            Queue<List<Info>> cells = new Queue<List<Info>>();
+            cells.Enqueue(mastersCells);
+
+            return cells;
+        }
+
+        private static Queue<List<Info>> create_Master_Queue()
+        {
+            List<Info> shapeCells = new List<Info>()
+            {
+                new Info("PinX", "1"),
+                new Info("PinY", "0.5"),
+                new Info("Width", "2"),
+                new Info("Height", "1"),
+                new Info("LocPinX", "1"),
+                new Info("LocPinY", "0.5"),
+                new Info("Angle", "0"),
+                new Info("FillForegnd", "#0070c0"),
+                new Info("FillBkgnd", "#000000")
+            };
+
+            List<Info> geometryCells = new List<Info>()
+            {
+                new Info("NoFill", "0"),
+                new Info("NoLine", "0"),
+                new Info("NoShow", "0")
+            };
+
+            List<Info> x1Cells = new List<Info>()
+            {
+                new Info("X", "0"),
+                new Info("Y", "0")
+            };
+
+            List<Info> x2Cells = new List<Info>()
+            {
+                new Info("X", "1"),
+                new Info("Y", "0")
+            };
+
+            List<Info> x3Cells = new List<Info>()
+            {
+                new Info("X", "1"),
+                new Info("Y", "1")
+            };
+
+            List<Info> x4Cells = new List<Info>()
+            {
+                new Info("X", "0"),
+                new Info("Y", "1")
+            };
+
+            List<Info> x5Cells = new List<Info>()
+            {
+                new Info("X", "0"),
+                new Info("Y", "0")
+            };
+
+            Queue<List<Info>> cells = new Queue<List<Info>>();
+            cells.Enqueue(shapeCells);
+            cells.Enqueue(geometryCells);
+            cells.Enqueue(x1Cells);
+            cells.Enqueue(x2Cells);
+            cells.Enqueue(x3Cells);
+            cells.Enqueue(x4Cells);
+            cells.Enqueue(x5Cells);
 
             return cells;
         }
@@ -225,15 +324,82 @@ namespace visio_test_app
             return writer;
         }
 
-        private static XmlWriter create_Page_Part()
+        private static XmlWriter create_Page_Part(Queue<List<Info>> cells)
         {
-            XmlWriter writer = xml_Writer_Init("page.xml", "Page", xml_Settings_Init());
-
+            XmlWriter writer = xml_Writer_Init("page.xml", "PageContents", xml_Settings_Init());
+            writer.WriteStartElement("Shapes");
+            writer.WriteStartElement("Shape");
+            writer.WriteAttributeString(null, "ID", null, "1");
+            writer.WriteAttributeString(null, "Type", null, "Shape");
+            writer.WriteAttributeString(null, "Master", null, "2");
+            writer = create_Cells(writer, cells.Dequeue());
+            writer.Close();
             return writer;
         }
 
+        private static XmlWriter create_Masters_Part(Queue<List<Info>> cells)
+        {
+            XmlWriter writer = xml_Writer_Init("Masters.xml", "Masters", xml_Settings_Init());
+            writer.WriteStartElement("Master");
+            writer.WriteAttributeString(null, "ID", null, "2");
+            writer.WriteAttributeString(null, "UniqueID", null, "{0020640D-0002-0000-8E40-00608CF305B2}");
+            writer.WriteAttributeString(null, "BaseID", null, "{{8FEBAF3E-100E-46A7AFB4-6F1C4D75A7A7}");
+            writer.WriteAttributeString(null, "PatternFlags", null, "0");
+            writer.WriteStartElement("PageSheet");
+            writer.WriteAttributeString(null, "LineStyle", null, "0");
+            writer.WriteAttributeString(null, "TextStyle", null, "0");
+            writer = create_Cells(writer, cells.Dequeue());
+            writer.WriteStartElement("Rel");
+            writer.WriteAttributeString("r", "id", null, "rId1");
+            writer.Close();
+            return writer;
+        }
 
-
+        private static XmlWriter create_Master_Part(Queue<List<Info>> cells)
+        {
+            XmlWriter writer = xml_Writer_Init("Master.xml", "MasterContents", xml_Settings_Init());
+            writer.WriteStartElement("Shapes");
+            writer.WriteAttributeString(null, "ID", null, "5");
+            writer.WriteAttributeString(null, "Type", null, "Shape");
+            writer.WriteAttributeString(null, "LineStyle", null, "3");
+            writer.WriteAttributeString(null, "FillStyle", null, "3");
+            writer.WriteAttributeString(null, "TextStyle", null, "3");
+            writer = create_Cells(writer, cells.Dequeue());
+            writer.WriteStartElement("Section");
+            writer.WriteAttributeString(null, "N", null, "Geometry");
+            writer.WriteAttributeString(null, "IX", null, "0");
+            writer = create_Cells(writer, cells.Dequeue());
+            writer.WriteStartElement("Row");
+            writer.WriteAttributeString(null, "T", null, "RelMoveTo");
+            writer.WriteAttributeString(null, "IX", null, "1");
+            writer = create_Cells(writer, cells.Dequeue());
+            writer.WriteEndElement();
+            writer.WriteStartElement("Row");
+            writer.WriteAttributeString(null, "T", null, "RelMoveTo");
+            writer.WriteAttributeString(null, "IX", null, "2");
+            writer = create_Cells(writer, cells.Dequeue());
+            writer.WriteEndElement();
+            writer.WriteStartElement("Row");
+            writer.WriteAttributeString(null, "T", null, "RelMoveTo");
+            writer.WriteAttributeString(null, "IX", null, "3");
+            writer = create_Cells(writer, cells.Dequeue());
+            writer.WriteEndElement();
+            writer.WriteStartElement("Row");
+            writer.WriteAttributeString(null, "T", null, "RelMoveTo");
+            writer.WriteAttributeString(null, "IX", null, "4");
+            writer = create_Cells(writer, cells.Dequeue());
+            writer.WriteEndElement();
+            writer.WriteStartElement("Row");
+            writer.WriteAttributeString(null, "T", null, "RelMoveTo");
+            writer.WriteAttributeString(null, "IX", null, "5");
+            writer = create_Cells(writer, cells.Dequeue());
+            writer.WriteEndElement();
+            writer.WriteEndElement();
+            writer.WriteStartElement("Text");
+            writer.WriteValue("Sample Text");
+            writer.Close();
+            return writer;
+        }
         static private void mapShapes(WorkFlowElement workFlow)
         {
 
